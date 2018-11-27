6c68b687f8f081cbfa69037afa579705cdd67a90
johan <johan@baboons.se>
2018-11-27 21:40:25 +0100

    First version

---
+++ README.md
@@ -0,0 +1,13 @@
+## Usage
+
+To install the latest version of Next Routes for Now CLI, run this command:
+
+```
+npm install -g next-routes-now
+```
+
+## Generate routes
+
+```
+generate-now-routes <routes.js> <now.json>
+```
---
+++ src/index.js
@@ -0,0 +1,30 @@
+const fs = require("fs");
+const path = require("path");
+
+const args = process.argv.slice(2);
+const nextRoutesFile = path.resolve(process.cwd(), args[0]);
+const nowConfigFile = path.resolve(process.cwd(), args[1]);
+
+const nextRoutes = require(nextRoutesFile);
+const nowConfig = require(nowConfigFile);
+
+if (!nowConfig.routes) {
+  nowConfig.routes = [];
+}
+
+nextRoutes.routes.forEach(route => {
+  if (nowConfig.routes.filter(e => e.src === route.regex.source).length === 0) {
+    nowConfig.routes.push({
+      src: route.regex.source,
+      dest: route.page
+    });
+  }
+});
+
+fs.writeFile(nowConfigFile, JSON.stringify(nowConfig, false, 2), err => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log("now.json has been updated");
+});
